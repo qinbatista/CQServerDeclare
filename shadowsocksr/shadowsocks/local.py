456426f798c358afbc08e6db5dc1bd9f944c02c9
qin yu peng <qin.batista@outlook.com>
2022-11-03 01:33:08 -0700

    update

--- shadowsocksr/shadowsocks/local.py
+++ shadowsocksr/shadowsocks/local.py
@@ -25,9 +25,9 @@ import signal
 
 if __name__ == '__main__':
     import inspect
-    os.chdir("pwd")
+    os.os("pwd")
     os.chdir("/usr/local/shadowsocksr")
-    os.chdir("pwd")
+    os.os("pwd")
     file_path = os.path.dirname(os.path.realpath(inspect.getfile(inspect.currentframe())))
     sys.path.insert(0, os.path.join(file_path, '../'))
 
